e2f736ad7972273dfb49a6f5e4a6cada58dce004
eng_monacherif <eng_monacherif@yahoo.com>
2010-06-29 16:48:22 +0000

    zawwedt 7aga fi class WelcomeForm

--- SIGNlator/WelcomeForm.cs
+++ SIGNlator/WelcomeForm.cs
@@ -21,5 +21,25 @@ namespace SIGNlator
         {
 
         }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // WelcomeForm
+            // 
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Name = "WelcomeForm";
+            this.Load += new System.EventHandler(this.WelcomeForm_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        private void WelcomeForm_Load_1(object sender, EventArgs e)
+        {
+
+        }
+        private void Choose_Language(int languageNo)
+        { }
     }
 }
